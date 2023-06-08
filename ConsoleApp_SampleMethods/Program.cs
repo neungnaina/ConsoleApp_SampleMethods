6c3c5f6e12b42b2c4cfdae762373bcc2f674c1c1
neungnaina <naiasdf@Gmail.com>
2023-06-08 11:43:02 +0700

    Add project files.

---
+++ ConsoleApp_SampleMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SampleMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program ObjProg = new Program();
+            ObjProg.SampleMethod();// Call object method
+            SampleMethod01(); // Call Static Methods
+        }
+        public void SampleMethod()
+        {
+            Console.WriteLine("Capybara");
+            Console.ReadKey();
+        }
+        static void SampleMethod01()
+        {
+            Console.WriteLine("Capybara");
+            Console.ReadKey();
+        }
+    }
+}
